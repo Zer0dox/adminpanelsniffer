cd9d8f027c7b4b38fc350ac5f56ba3e278d3544a
Zyantos <99581531+Zer0dox@users.noreply.github.com>
2023-07-21 09:57:57 -0700

    Update adminfinder.pl
    Improved accuracy

--- adminfinder.pl
+++ adminfinder.pl
@@ -5,6 +5,8 @@ use LWP::UserAgent;
 use Term::ANSIColor;
 use threads;
 use Thread::Queue;
+use Term::ProgressBar;
+use HTML::Parser;
 
 my ($target, $wordlist) = @ARGV;
 my $ua = LWP::UserAgent->new();
@@ -23,6 +25,9 @@ my %status_colors = (
 my @ignored_status_codes = (404);
 my @ignored_urls = ("http://$target/favicon.ico");
 
+# Keywords to detect valid admin panels
+my @admin_panel_keywords = ("admin", "dashboard", "login", "controlpanel");
+
 system("clear");
 print_title();
 
@@ -34,6 +39,13 @@ unless (defined $target and defined $wordlist) {
 
 open my $wordlist_file, '<', $wordlist or die("Error: Unable to open the file $wordlist");
 
+# Count the number of lines in the wordlist file for the progress bar
+my $total_lines = `wc -l $wordlist | cut -d' ' -f1`;
+chomp $total_lines;
+
+# Start progress bar
+my $progress = Term::ProgressBar->new({ count => $total_lines, name => 'Scanning', ETA => 'linear' });
+
 # Start worker threads
 for (1..$thread_count) {
     threads->create(\&worker_thread);
@@ -44,6 +56,7 @@ while (my $line = <$wordlist_file>) {
     chomp $line;
     my $url = construct_url($target, $line);
     $url_queue->enqueue($url);
+    $progress->update($.) if $progress->needs_update;
 }
 
 close $wordlist_file;
@@ -54,7 +67,7 @@ $url_queue->enqueue(undef) for 1..$thread_count;
 # Wait for all threads to finish
 $_->join() for threads->list();
 
-print "Valid Pages\n------------------------------------------\n";
+print "\nValid Pages\n------------------------------------------\n";
 print join("\n", @found) . "\n";
 
 sub worker_thread {
@@ -63,7 +76,13 @@ sub worker_thread {
         my $status_code = get_url_status($ua, $url);
         next if is_ignored_status($status_code, $url);
         print_status($url, $status_code);
-        push(@found, $url) if is_valid_status($status_code);
+
+        if (is_valid_status($status_code)) {
+            my $html_content = get_url_content($ua, $url);
+            if (has_admin_panel_keywords($html_content)) {
+                push(@found, $url);
+            }
+        }
     }
 }
 
@@ -76,6 +95,12 @@ sub get_url_status {
     my ($ua, $url) = @_;
     my $response = $ua->get($url);
     return $response->code;
+}
+
+sub get_url_content {
+    my ($ua, $url) = @_;
+    my $response = $ua->get($url);
+    return $response->content;
 }
 
 sub print_status {
@@ -95,6 +120,14 @@ sub is_ignored_status {
     my ($status_code, $url) = @_;
     return 1 if grep { $_ == $status_code } @ignored_status_codes;
     return 1 if grep { $url eq $_ } @ignored_urls;
+    return 0;
+}
+
+sub has_admin_panel_keywords {
+    my $html_content = shift;
+    foreach my $keyword (@admin_panel_keywords) {
+        return 1 if $html_content =~ /\b$keyword\b/i;
+    }
     return 0;
 }
 
